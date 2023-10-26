f4ff8aacb278d67dcd1e65eb062fa41348f4f349
kimmingye <dusgml3779@gmail.com>
2023-10-26 13:41:15 +0900

    Vue CLI & Todo App

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Todo-Header></Todo-Header>
+    <Todo-Input></Todo-Input>
+    <Todo-List></Todo-List>
+    <Todo-Footer></Todo-Footer>
+  </div>
+</template>
+
+<script>
+
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from "./components/TodoInput.vue"
+import TodoList from "./components/TodoList.vue"
+import TodoFooter from "./components/TodoFooter.vue"
+
+export default {
+  components : {
+    'Todo-Header' : TodoHeader,
+    'Todo-Input' : TodoInput,
+    'Todo-List' : TodoList,
+    'Todo-Footer' : TodoFooter
+  }
+}
+</script>
+
+<style>
+
+</style>
