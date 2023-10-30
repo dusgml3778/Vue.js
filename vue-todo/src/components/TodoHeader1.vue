f635c2d8a85b083fe8e2e98e94bfb7d35b57d69c
kimmingye <dusgml3779@gmail.com>
2023-10-30 18:39:05 +0900

    todo app practice

---
+++ vue-todo/src/components/TodoHeader1.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <header>
+      <h1>TODO it!</h1>
+    </header>
+  </div>
+</template>
+
+<style>
+h1 {
+  color: #2f3852;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
--- vue-todo/src/components/TodoList1.vue
+++ vue-todo/src/components/TodoList1.vue
@@ -1,16 +1,14 @@
 <template>
-  <div>
-    <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
-        <i class = "checkBtn fas fa-check" v-bind:class="{checkBtnCompleted:todoItem.completed}" 
-        v-on:click="toggleComplete(todoItem, index)"></i>
-        <span v-bind:class="{textCompleted:todoItem.completed}">{{ todoItem.item }}</span>
-        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
-          <i class="removeBtn fas fa-trash-alt"></i>
-        </span>
-      </li>
-    </ul>
-  </div>
+  <ul>
+    <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class ="shadow">
+      <i class ="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted:todoItem.completed}"
+      v-on:click="toggleComplete(todoItem, index)"></i>
+      <span v-bind:class="{textCompleted:todoItem.completed}">{{ todoItem.item }}</span>
+      <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+        <i class="removeBtn fas fa-trash-alt"></i>
+      </span>
+    </li>
+  </ul> 
 </template>
 
 <script>
@@ -21,21 +19,21 @@ export default {
     }
   },
   methods: {
-    removeTodo:function(todoItem, index){
+    removeTodo:function(todoItem, index) {
       localStorage.removeItem(todoItem);
       this.todoItems.splice(index,1);
     },
-    toggleComplete:function(todoItem, index){
+    toggleComplete:function(todoItem, index) {
       todoItem.completed = !todoItem.completed;
       localStorage.removeItem(todoItem.item);
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+      localStorage.setItem(todoItem.item,JSON.stringify(todoItem));
     }
   },
-  created:function(){
-    if(localStorage.length>0){
+  created:function() {
+    if(localStorage.length>0) {
       for(var i = 0;i<localStorage.length;i++) {
         if((localStorage.key(i) !=="loglevel:webpack-dev-server")){
-              this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));        
         }
       }
     }
