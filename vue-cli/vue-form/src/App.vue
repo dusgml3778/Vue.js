763af0da6df7aa6a730f8b6c20b988025ba71ec2
kimmingye <dusgml3779@gmail.com>
2023-10-31 16:13:54 +0900

    refactoring

---
+++ vue-cli/vue-form/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+<form v-on:submit.prevent="submitForm">
+  <div>
+    <label for="username">id: </label>
+    <input type="text" id="username" v-model="username">
+  </div>
+  <div>
+    <label for="password">pw: </label>
+    <input type="text" id="password" v-model="password">
+  </div>
+  <button type="submit">Login</button>
+</form>
+  
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data:function() {
+    return {
+      username:"",
+      password : ""
+    }
+  },
+  methods: {
+    submitForm:function(){
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data).then(function(res){
+        console.log(res);
+      }).catch(function(error){
+        console.log(error);
+      });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-todo/src/App1.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <Todo-Header1></Todo-Header1>
+    <Todo-Input1></Todo-Input1>
+    <Todo-List1></Todo-List1>
+    <Todo-Footer1></Todo-Footer1>
+
+  </div>
+</template>
+
+<script>
+
+import TodoHeader1 from "./components/TodoHeader1.vue"
+import TodoInput1 from "./components/TodoInput1.vue"
+import TodoList1 from "./components/TodoList1.vue"
+import TodoFooter1 from "./components/TodoFooter1.vue"
+
+export default {
+  components : {
+    "Todo-Header1" : TodoHeader1,
+    "Todo-Input1" : TodoInput1,
+    "Todo-List1" : TodoList1,
+    "Todo-Footer1" : TodoFooter1
+  }
+
+}
+</script>
+
+<style>
+  body {
+    text-align: center;
+    background-color: #f6f6f6;
+  }
+
+  input {
+    border-style: groove;
+    widows: 200px;
+  }
+
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+
+</style>
